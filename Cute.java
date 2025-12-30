9b3a8b510242184230619f8b53eed4e81e17b86e
Vallirie <jecynvallirieturbanos@gmail.com>
2025-12-30 19:46:01 +0800

    Add comment in Cute.java

--- Cute.java
+++ Cute.java
@@ -1,5 +1,6 @@
 class Cute{
     public static void main(String[] args) {
+        //HAHAHAH
             System.out.println("Hello World!!");
             System.out.println("Testing!!");
             System.out.println("Trial is here!!");
